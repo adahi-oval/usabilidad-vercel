3ffa45546ec7b93ec3b0d7d92924b781b4a20d84
Adahi Oval <alu0101242071@ull.edu.es>
2023-07-10 19:34:58 +0100

    vercel

--- src/components/BotonEmail.js
+++ src/components/BotonEmail.js
@@ -2,16 +2,8 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 
 const BotonEmail = () => {
-  const emailAddress = 'alu0101242071@ull.edu.es';
-  const subject = 'Solicitud Contacto ESTUAP';
-  const body = 'Contenido del email';
 
   const handleEmailClick = () => {
-    const mailtoLink = `mailto:${emailAddress}?subject=${encodeURIComponent(
-      subject
-    )}&body=${encodeURIComponent(body)}`;
-
-    window.location.href = mailtoLink;
   };
 
   return (
